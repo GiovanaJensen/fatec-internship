f4a7232773b2393e2b43f33a39c4bf7af8f8dae5
GiovanaJensen <oliveirasgiovana143@gmail.com>
2024-04-13 17:32:05 -0300

    fix: padding dos pontos e cor de fundo

--- lib/components/Header.dart
+++ lib/components/Header.dart
@@ -14,36 +14,50 @@ class Header extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text(title, style: Theme.of(context).textTheme.bodyLarge,),
-          Row(
-            children: <Widget>[
-              Container(
-                width: 23,
-                height: 23,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: ThemeColors.primaryColor
-                ),
-                child: Icon(Icons.star, color: Colors.white, size: 11,),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 13),
-                child: Text.rich(
-                  TextSpan(
-                    text: "100",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 12,
+          Text(
+            title,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(120)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 23,
+                    height: 23,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: ThemeColors.primaryColor),
+                    child: const Icon(
+                      Icons.star,
+                      color: Colors.white,
+                      size: 11,
                     ),
-                    children: <TextSpan> [ TextSpan(
-                      text: " pontos", 
-                      style: TextStyle(fontWeight: FontWeight.normal),
-                    ),]
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 13),
+                    child: Text.rich(TextSpan(
+                        text: "100",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 12,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: " pontos",
+                            style: TextStyle(fontWeight: FontWeight.normal),
+                          ),
+                        ])),
                   )
-                ),
-              )
-            ],
+                ],
+              ),
+            ),
           )
         ],
       ),
